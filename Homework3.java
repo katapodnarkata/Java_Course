cbd0070c5a62123ff2a3bc81defbf3317cd38a02
katapodnarkata <katapodnarkata@gmail.com>
2023-07-12 13:09:50 +0300

    Change repository and chand HW 3

---
+++ Homework3.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Homework3 {
+    public static void main(String args[]){
+		Scanner scum = new Scanner(System.in);
+
+        String username = "Admin";
+        String password = "P@ssword";
+
+        System.out.println("Enter username ");
+        String name = scum.nextLine();
+
+          if (!name.equals(username))
+        {
+          System.out.println("No user with this name");
+        }
+        else
+        {
+          System.out.println("Enter password:");
+          String pass = scum.nextLine();
+         while (!pass.equals(password)) {
+          System.out.println("Incorrect password");
+          System.out.println("Enter password:");  
+        pass = scum.nextLine(); 
+        scum.close();
+        }
+        System.out.println("Hi, "+username+"! Welcome to the club buddy");
+        }
+    }
+}
